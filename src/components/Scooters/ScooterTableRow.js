2be970c503348088aa7c79be618482221d67cb32
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2022-11-15 04:57:21 +0300

    feat: Implement JavaScript functionality (ScooterTableRow.js)

---
+++ src/components/Scooters/ScooterTableRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ScooterTableRow = ({ scooter }) => {
+  return(
+    <tr>
+      <td><input type="checkbox"/></td>
+      <td>{ scooter.id }</td>
+      <td>{ scooter.vin }</td>
+      <td>{ scooter.model }</td>
+      <td>{ scooter.market_id }</td>
+      <td>{ scooter.energy_level } %</td>
+      <td>{ scooter.license_plate }</td>
+      <td>{ scooter.distance_to_travel }</td>
+      <td> { scooter.location.lng } </td>
+      <td> { scooter.location.lat } </td>
+    </tr>
+  );
+}
+
+export default ScooterTableRow;
